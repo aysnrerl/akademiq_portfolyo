c29766d2480a98cda95db98bb9da6bc3accf6154
aysnrerl <134006908+aysnrerl@users.noreply.github.com>
2025-11-19 20:14:38 +0300

    Add project files.

---
+++ akademiq_portfolyo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace akademiq_portfolyo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
